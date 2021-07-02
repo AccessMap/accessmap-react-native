b7ad85e7b14f2d51d0618a7d343381758fb46f37
Jay Lin <jial8@uw.edu>
2021-07-02 12:21:19 -0700

    attempt

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React from 'react';
-import { Animated, Easing } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { Animated, Easing } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import MapPage from './navigation/MapPage';
-import SearchPage from './navigation/SearchPage';
-import Crowdsourcing from './navigation/Crowdsourcing';
+import MapPage from "./navigation/MapPage";
+import SearchPage from "./navigation/SearchPage";
+import Crowdsourcing from "./navigation/Crowdsourcing";
 
-const MainNavigator = createStackNavigator({
-	Map: {screen: MapPage},
-	Crowdsourcing: {screen: Crowdsourcing},
-	Search: {screen: SearchPage},
-	}, {
-		transitionConfig : () => ({
-			transitionSpec: {
-				duration: 0,
-				timing: Animated.timing,
-				easing: Easing.step0,
-			},
-		}, {
-			// initialRouteName: "Map",
-			initialRouteName: "Crowdsourcing",
-		}),
-	});
+const Stack = createStackNavigator();
 
-const App = createAppContainer(MainNavigator);
+// Transition in and out screen animations
+const transitionConfig = () => {
+  return {
+    transitionSpec: {
+      duration: 200,
+      timing: Animated.timing,
+      easing: Easing.step0,
+    },
+  };
+};
 
-export default App;
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen name="Map" component={MapPage} />
+        <Stack.Screen name="Crowdsourcing" component={Crowdsourcing} />
+        <Stack.Screen name="Search" component={SearchPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
