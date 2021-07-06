26e87876fa6e8f3bd68356b784409091cbbe9384
yehric2018 <eric.yehj@outlook.com>
2021-07-05 20:14:06 -0700

    New navigation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import { Animated, Easing } from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { Animated, Easing, TextInput, View } from 'react-native';
+import * as React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import MapPage from './navigation/MapPage';
 import SearchPage from './navigation/SearchPage';
 import Crowdsourcing from './navigation/Crowdsourcing';
 
-const MainNavigator = createStackNavigator({
-	Map: {screen: MapPage},
-	Crowdsourcing: {screen: Crowdsourcing},
-	Search: {screen: SearchPage},
-	}, {
-		transitionConfig : () => ({
-			transitionSpec: {
-				duration: 0,
-				timing: Animated.timing,
-				easing: Easing.step0,
-			},
-		}, {
-			// initialRouteName: "Map",
-			initialRouteName: "Crowdsourcing",
-		}),
-	});
+const Stack = createStackNavigator();
 
-const App = createAppContainer(MainNavigator);
+// Transition in and out screen animations
+const transitionConfig = () => {
+	return {
+		transitionSpec: {
+			duration: 200,
+			timing: Animated.timing,
+			easing: Easing.step0,
+		},
+	};
+};
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				initialRouteName="Map"
+				screenOptions={{
+					transitionSpec: {
+						open: transitionConfig,
+						close: transitionConfig,
+					},
+				}}>
+				<Stack.Screen
+					name="Map"
+					component={MapPage}
+					options={{
+						headerShown: false,
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
 
 export default App;
