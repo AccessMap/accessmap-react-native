dae5daf722d7c9101d6af94341787bd7f927036f
Jay Lin <jial8@uw.edu>
2021-07-18 19:16:05 -0700

    very basic How To Navigate tutorial finished
    1. no translations yet. all in english

--- src/components/Tutorials/ToolTip.js
+++ src/components/Tutorials/ToolTip.js
@@ -1,41 +1,40 @@
 //------------------------------------------------------------------------------------------
 // Custom tooltip used in tutorial onboarding screens
-import React, {useState} from "react";
+import React from "react";
 import { View, Text, Button } from "react-native";
 
-// [string] toolTipPositionLeft: specify percentage from the left boundary of the screen
-// [string] toolTipPositionTop: percentage from the top boundary of the screen
-// [boolean] arrowUp: true if the arrow should face up
-
-// [string] color: background color of the entire tooltip
-// [string] textColor: color of the text
-
-// [string] cardDescription: generally describes the card (ex: Route Planning)
-// [int] maxStep: how many total steps there are
-
-// [string] nextButtonText: (ex: 'Next' or 'End') text to be placed in the right button
-// [string] nextStep: function to execute when pressing the 'Next'/'End' Button
 export default function ToolTip({
-    toolTipPositionLeft, toolTipPositionTop, 
-    backgroundColor, textColor, cardDescription, maxStep,
-    nextButtonColor, toolTipVariables}) {
-  const [numStep, goToNextStep] = useState(0);
-
-  console.log(numStep);
+    backgroundColor, // [string] background color of the entire tooltip
+    textColor, // [string] color of the text
+    cardDescription, // [string] generally describes the card (ex: Route Planning)
+    numStep, // [int] current step 
+    maxStep, // [int] how many total steps there are
+    nextButtonColor,
+    toolTipPositionLeft, // [string] percentage from the left boundary of the screen
+    toolTipPositionTop, // [string] percentage from the top boundary of the screen
+    arrowPositionLeft,
+    arrowPositionTop,
+    heading, // [string] bold heading text
+    paragraph, // [string] smaller detail text
+    showBack, // [boolean] show the back button
+    nextButtonText, // [string] (ex: 'Next' or 'End') text to be placed in the right button
+    goToNextStep, // [function] executed when pressing the 'Next'/'End' Button
+    navigation, // [navigation] ability to close the current screen
+  }) {
 
   return (
     <View style={{
         position:"absolute", 
-        left: "20%",
-        top: "17%",
+        left: toolTipPositionLeft,
+        top: toolTipPositionTop,
     }}>
         
       <View // tooltip arrow
         accessible={false}
         style={{
           position: "relative",
-          left: "50%",
-          top: "-20%",
+          left: arrowPositionLeft,
+          top: arrowPositionTop,
           width: 30,
           height: 30,
           backgroundColor: backgroundColor,
@@ -47,8 +46,6 @@ export default function ToolTip({
       <View // tooltip rectangle box
         style={{ 
             position: "absolute", 
-            left: toolTipPositionLeft, 
-            top: toolTipPositionTop, 
             zIndex: 99 
         }}
       >
@@ -78,15 +75,15 @@ export default function ToolTip({
               marginBottom: 10,
             }}
           >
-            {toolTipVariables[numStep].heading}
+            {heading}
           </Text>
           <Text style={{ marginBottom: 20, color: textColor }}>
-            { toolTipVariables[numStep].paragraph }
+            { paragraph }
           </Text>
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
-            <View style={{ width: 100, opacity: toolTipVariables[numStep].showBack ? 1.0 : 0.0 }}>
+            <View style={{ width: 100, opacity: showBack ? 1.0 : 0.0 }}>
               <Button
                 title="Back"
                 color={backgroundColor}
@@ -95,9 +92,17 @@ export default function ToolTip({
             </View>
             <View style={{ width: 100}}>
               <Button
-                title="Next"
+                title={nextButtonText}
                 color={nextButtonColor}
-                onPress={() => goToNextStep(numStep + 1)}
+                onPress={() => { 
+                  console.log("nS=" + numStep);
+                  if (numStep >= maxStep - 1) {
+                    console.log("closing nav");
+                    navigation.goBack();
+                  } else {
+                    goToNextStep(numStep + 1) 
+                  }
+                }}
               />
             </View>
           </View>
--- src/navigation/TutorialPage.js
+++ src/navigation/TutorialPage.js
@@ -1,59 +1,69 @@
 // A TutorialPage shows a series of still images of the app, guiding the user through
 // main functionalities. Includes a custom tooltip shape.
-import React from "react";
+import React, {useState} from "react";
 import { View, Image, AccessibilityInfo, Text, Button } from "react-native";
 import ToolTip from "../components/Tutorials/ToolTip";
 
 export default function TutorialPage({ route, navigation }) {
-  const maxSteps = 5;
+  const [numStep, goToNextStep] = useState(0);
+  const maxSteps = 4;
   const textColor = "white";
   const cardDescription = "Route Planning";
   const backgroundColor ="#0F47A1";
   const nextButtonColor = "#0164FF";
 
   const toolTipVariables = [
-    { // First Card: Choose a destination with Search Bar
+    { // Choose a destination with Search Bar
       "captions": 
       "Search bar located at the top of the app. Gray text inside the bar reads: Enter Address", 
-      "toolTipPositionLeft":"15%", "toolTipPositionTop":"18%",
-      "arrowPositionLeft":"50%", "arrowPositionTop":"-20%",
+      "toolTipPositionLeft":"17%", "toolTipPositionTop":"17%",
+      "arrowPositionLeft":"50%", "arrowPositionTop":"-25%",
       "heading": "Find Directions",
       "paragraph":
-      "Choose the start or destination of your route by tapping on the map or entering a location via the Search Bar.",
+      "Set a route start or destination by tapping on the map or entering a location in the Search Bar.",
       "showBack": false,
+      "nextButtonText":"Next",
+    },
+    { // Grey enter icon to pick start and end
+      "captions": "A gray pencil icon button next to the Mobility Mode buttons.",
+      "toolTipPositionLeft":"35%", "toolTipPositionTop":"19%",
+      "arrowPositionLeft":"595%", "arrowPositionTop":"-25%",
+      "heading": "Find Directions",
+      "paragraph":
+      "Alternatively, tap on this grey icon to manually input a start and destination.",
+      "showBack": true,
       "nextButtonText":"Next",
     },
     { // Mobility Buttons
       "captions": 
       "Mobility buttons located under the search bar in order: Custom, Wheelchair, Powered, and Cane.", 
-      "toolTipPositionLeft":"15%", "toolTipPositionTop":"18%",
-      "arrowPositionLeft":"50%", "arrowPositionTop":"-10%%",
-      "heading": "Choose Mobility Setting",
+      "toolTipPositionLeft":"13%", "toolTipPositionTop":"25%",
+      "arrowPositionLeft":"50%", "arrowPositionTop":"-25%",
+      "heading": "Mobility Profile",
       "paragraph":
-      "Choose the mobility setting that best fits your transportation needs. This adjusts the routing by looking at the steepness level of sidewalks.",
+      "Customize your travel by selecting the mode of transportation that best fits you. As an example, the Custom Profile is currently selected.",
       "showBack": true,
       "nextButtonText":"Next",
     },
     { // Mobility Buttons
-      "captions": 
-      "3", 
-      "toolTipPositionLeft":"15%", "toolTipPositionTop":"18%",
-      "arrowPositionLeft":"50%", "arrowPositionTop":"-10%%",
-      "heading": "Choose Mobility Setting",
+      "captions": "A gray pencil icon button next to the Mobility Mode buttons.",
+      "toolTipPositionLeft":"20%", "toolTipPositionTop":"25%",
+      "arrowPositionLeft":"550%", "arrowPositionTop":"-25%",
+      "heading": "Mobility Profile",
       "paragraph":
-      "he steepness level of sidewalks.",
+      "While in the Custom Profile setting, tap on the pencil icon to adjust route characteristics such as sidewalk steepness.",
       "showBack": true,
-      "nextButtonText":"Next",
+      "nextButtonText":"End Tour",
     },
   ];
+
+  console.log("numStep = " + numStep);
 
   return (
     <View style={{ width: "100%" }}>
       <Image
-        accessibilityLabel={
-          "Search bar located at the top of the app, highlighted. Gray text inside the bar reads: Enter Address"
-        }
-        style={{ height: "100%", width: "100%", margin: 0, padding: 0, opacity: 0.75 }}
+        accessibilityLabel={toolTipVariables[numStep].captions}
+        style={{ height: "100%", width: "100%", margin: 0, padding: 0, opacity: 0.65 }}
         source={require("../../assets/tutorial_background.png")}
         resizeMode="contain"
         resizeMethod="scale"
@@ -63,9 +73,19 @@ export default function TutorialPage({ route, navigation }) {
         backgroundColor={backgroundColor}
         textColor={textColor}
         cardDescription={cardDescription}
+        numStep={numStep}
         maxStep={maxSteps}
         nextButtonColor={nextButtonColor}
-        toolTipVariables={toolTipVariables}
+        toolTipPositionLeft={toolTipVariables[numStep].toolTipPositionLeft}
+        toolTipPositionTop={toolTipVariables[numStep].toolTipPositionTop}
+        arrowPositionLeft={toolTipVariables[numStep].arrowPositionLeft}
+        arrowPositionTop={toolTipVariables[numStep].arrowPositionTop}
+        heading={toolTipVariables[numStep].heading}
+        paragraph={toolTipVariables[numStep].paragraph}
+        showBack={toolTipVariables[numStep].showBack}
+        nextButtonText={toolTipVariables[numStep].nextButtonText}
+        goToNextStep={goToNextStep}
+        navigation={navigation}
       />
     </View>
   );
