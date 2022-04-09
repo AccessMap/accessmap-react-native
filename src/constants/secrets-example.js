9931f45ea0d94ee1c53c2b3c94a63541beb2cfc0
Jay Lin <jial8@uw.edu>
2022-04-09 12:51:30 -0700

    Create secrets-example.js

---
+++ src/constants/secrets-example.js
@@ -0,0 +1,13 @@
+/**
+ * Application Secrets
+ */
+
+// Google Sheets (Feedback Form)
+export const spreadsheetId = "";
+export const accountId = "";
+export const accountName = "";
+export const keyId = "";
+export const key = "";
+
+// User Data Analytics
+export const postHogAPIKey = "";
